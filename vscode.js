4fb4cf68659e255867189d16ddb70b923de73978
Christopher Wavrin <cwavrin@lineate.com>
2019-12-12 20:54:51 -0600

    adding bash and vscode

---
+++ vscode.js
@@ -0,0 +1,38 @@
+{
+  "editor.tabSize": 2,
+  "terminal.integrated.shell.osx": "/bin/bash",
+  "workbench.settings.editor": "json",
+  "workbench.settings.useSplitJSON": true,
+  "workbench.iconTheme": "vscode-icons",
+  // font setup
+  "editor.fontFamily": "Dank Mono, Menlo, Monaco, 'Courier New', monospace",
+  "terminal.integrated.fontFamily": "Dank Mono",
+  "editor.fontLigatures": true,
+  "editor.fontSize": 16,
+  "editor.lineHeight": 23.2,
+  "editor.letterSpacing": 0.5,
+  "terminal.integrated.fontSize": 14,
+  "workbench.fontAliasing": "auto",
+  "vsicons.dontShowNewVersionMessage": true,
+  "workbench.colorTheme": "Night Owl",
+  // Cursor setup
+  "editor.cursorBlinking": "solid",
+  "editor.cursorWidth": 5,
+  // UI improvements
+  "editor.minimap.enabled": false,
+  "explorer.openEditors.visible": 1,
+  "workbench.startupEditor": "newUntitledFile",
+  "explorer.confirmDragAndDrop": false,
+  "workbench.editor.enablePreview": false,
+  "editor.snippetSuggestions": "top",
+  "editor.tabCompletion": "on",
+  "editor.quickSuggestionsDelay": 0,
+  "editor.multiCursorModifier": "ctrlCmd",
+  "terminal.integrated.macOptionIsMeta": true,
+  // Whitespace setup
+  "editor.renderWhitespace": "all",
+  "files.trimTrailingWhitespace": true,
+  "files.trimFinalNewlines": true,
+  "diffEditor.ignoreTrimWhitespace": false
+
+}
