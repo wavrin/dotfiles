0c5504e571bbdf475a809530ac30d7ca85d8c842
Christopher Wavrin <wavrin@gmail.com>
2020-11-28 22:53:33 -0600

    Update vscode.js

--- vscode.js
+++ vscode.js
@@ -1,60 +1,63 @@
 {
-  "editor.tabSize": 2,
-  "terminal.integrated.shell.osx": "/bin/bash",
-  "workbench.settings.editor": "json",
-  "workbench.settings.useSplitJSON": true,
-  "workbench.iconTheme": "vscode-icons",
-  // font setup
-  "editor.fontFamily": "Dank Mono, Menlo, Monaco, 'Courier New', monospace",
-  "terminal.integrated.fontFamily": "Dank Mono",
-  "editor.fontLigatures": true,
-  "editor.fontSize": 16,
-  "editor.lineHeight": 23,
-  "editor.letterSpacing": 0.5,
-  "terminal.integrated.fontSize": 16,
-  "workbench.fontAliasing": "auto",
-  "vsicons.dontShowNewVersionMessage": true,
-  "workbench.colorTheme": "Night Owl",
-  // Cursor setup
-  "editor.cursorBlinking": "solid",
-  "editor.cursorWidth": 5,
-  // UI improvements
-  "editor.minimap.enabled": false,
-  "explorer.openEditors.visible": 1,
-  "workbench.startupEditor": "newUntitledFile",
-  "explorer.confirmDragAndDrop": false,
-  "workbench.editor.enablePreview": false,
-  "editor.snippetSuggestions": "top",
-  "editor.tabCompletion": "on",
-  "editor.quickSuggestionsDelay": 0,
-  "editor.multiCursorModifier": "ctrlCmd",
-  "terminal.integrated.macOptionIsMeta": true,
-  // Whitespace setup
-  "editor.renderWhitespace": "all",
-  "files.trimTrailingWhitespace": true,
-  "files.trimFinalNewlines": true,
-  "files.insertFinalNewline": true,
-  "diffEditor.ignoreTrimWhitespace": false,
-  "macros": {
-      "collapseAndClose": [
-          "workbench.files.action.collapseExplorerFolders",
-          "workbench.action.closeAllEditors"
-      ]
-  },
-  // Git
-  "git.autofetch": true,
-  "git.enableSmartCommit": true,
-  // Gitlens
-  "gitlens.views.repositories.files.layout": "auto", // Updated since 9.0.0 - 2018-12-02.
-  "gitlens.currentLine.enabled": false,
-  "gitlens.hovers.enabled": false,
-  "gitlens.keymap": "alternate",
-  "gitlens.advanced.messages": {
-      "suppressShowKeyBindingsNotice": true
-  },
-  // Emmet
-  "emmet.triggerExpansionOnTab": true,
-  // Formatting
-  "editor.formatOnPaste": true,
-  "editor.formatOnSave": true
+    "editor.tabSize": 2,
+    "terminal.integrated.shell.osx": "/bin/zsh",
+    "workbench.settings.editor": "json",
+    "workbench.settings.useSplitJSON": true,
+    "workbench.iconTheme": "vscode-great-icons",
+    // font setup
+    "editor.fontFamily": "Dank Mono, Menlo, Monaco, 'Courier New', monospace",
+    "terminal.integrated.fontFamily": "Dank Mono",
+    "editor.fontLigatures": true,
+    "editor.fontSize": 16,
+    "editor.lineHeight": 23,
+    "editor.letterSpacing": 0.5,
+    "terminal.integrated.fontSize": 16,
+    "workbench.fontAliasing": "auto",
+    "vsicons.dontShowNewVersionMessage": true,
+    // Cursor setup
+    "editor.cursorBlinking": "solid",
+    "editor.cursorWidth": 5,
+    // UI improvements
+    "editor.minimap.enabled": false,
+    "explorer.openEditors.visible": 1,
+    "workbench.startupEditor": "newUntitledFile",
+    "explorer.confirmDragAndDrop": false,
+    "workbench.editor.enablePreview": false,
+    "editor.snippetSuggestions": "top",
+    "editor.tabCompletion": "on",
+    "editor.quickSuggestionsDelay": 0,
+    "editor.multiCursorModifier": "ctrlCmd",
+    "terminal.integrated.macOptionIsMeta": true,
+    // Whitespace setup
+    "editor.renderWhitespace": "all",
+    "files.trimTrailingWhitespace": true,
+    "files.trimFinalNewlines": true,
+    "files.insertFinalNewline": true,
+    "diffEditor.ignoreTrimWhitespace": false,
+    "macros": {
+        "collapseAndClose": [
+            "workbench.files.action.collapseExplorerFolders",
+            "workbench.action.closeAllEditors"
+        ]
+    },
+    // Git
+    "git.autofetch": true,
+    "git.enableSmartCommit": true,
+    // Gitlens
+    "gitlens.views.repositories.files.layout": "auto", // Updated since 9.0.0 - 2018-12-02.
+    "gitlens.currentLine.enabled": false,
+    "gitlens.hovers.enabled": false,
+    "gitlens.keymap": "alternate",
+    "gitlens.advanced.messages": {
+        "suppressShowKeyBindingsNotice": true
+    },
+    // Emmet
+    "emmet.triggerExpansionOnTab": true,
+    // Formatting
+    "editor.formatOnPaste": true,
+    "editor.formatOnSave": true,
+    "workbench.colorTheme": "Night Owl",
+    "aws.onDefaultRegionMissing": "add",
+    "window.zoomLevel": 1,
+    "aws.profile": "profile:default"
 }
